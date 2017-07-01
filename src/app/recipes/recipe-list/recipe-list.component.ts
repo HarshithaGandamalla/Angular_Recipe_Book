67d9d8b7017e033ac1918fb22a4e58bdaaa0e68a
Harshitha <harshithag15@gmail.com>
2017-07-01 12:15:26 -0400

    added ingredient model and setup recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  //recipe=[]; --without added type of Recipe array
+  recipes:Recipe[] = [
+    new Recipe('A test recipe','yum','https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Wikimedia-logo.svg/900px-Wikimedia-logo.svg.png')
+    ,new Recipe('A test recipe','yum','https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Wikimedia-logo.svg/900px-Wikimedia-logo.svg.png')
+  ];
 
-  ngOnInit() {
-  }
+   constructor(){
+
+   }
+
+
+     ngOnInit() {
+     }
 
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,19 @@
+
+/*export class ingredient{
+  public name: string;
+  public amount: number;
+
+   constructor(name: string,amount:number){
+     this.name=name;
+     this.amount=amount;
+   }
+
+} */
+
+export class ingredient{
+
+  //same effect as before
+  constructor(public name: string,public amount:number){
+
+   }
+ }
